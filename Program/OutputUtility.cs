c3c619282d365d820d94630def3e5484366ac55b
kacper119p <kacper119p@gmail.com>
2024-03-23 03:30:06 +0100

    Create OutputUtility.cs

---
+++ Program/OutputUtility.cs
@@ -0,0 +1,18 @@
+﻿using Pathfinding;
+
+namespace Program;
+
+public static class OutputUtility
+{
+    public static char DirectionToChar(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Up => 'U',
+            Direction.Down => 'D',
+            Direction.Left => 'L',
+            Direction.Right => 'R',
+            _ => throw new ArgumentException("Invalid Direction")
+        };
+    }
+}
